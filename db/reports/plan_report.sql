c45a5f1d20e56892fe8b9ed783403e1bb3a8139c
Georgi D. Sotirov <gdsotirov@gmail.com>
2011-11-15 11:12:14 +0200

    Add report for planned leaves that is required by law

---
+++ db/reports/plan_report.sql
@@ -0,0 +1,13 @@
+SELECT CONCAT(PER.first_name, ' ', PER.last_name) `Name`,
+       for_year `Year`,
+       MONTH(VAC.from_date) `Month`,
+       DATE_FORMAT(from_date, '%d/%m/%Y') `Date from`,
+       DATE_FORMAT(to_date, '%d/%m/%Y') `Date to`,
+       DATEDIFF(to_date, from_date) + 1 `Days`
+  FROM absences  VAC,
+       employees EMP,
+       persons   PER
+ WHERE VAC.type = 11
+   AND VAC.for_year = '2012'
+   AND EMP.id = VAC.employee_id
+   AND PER.id = EMP.person_id;
