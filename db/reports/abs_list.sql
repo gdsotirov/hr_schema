6acd6c4f92d516a09ec613f16a680e14a924e86f
Georgi D. Sotirov <gdsotirov@gmail.com>
2012-12-20 13:02:13 +0200

    List absences

---
+++ db/reports/abs_list.sql
@@ -0,0 +1,15 @@
+SELECT AB.id, ABT.title,
+       DATE_FORMAT(AB.from_date, '%d %b %Y') `From`,
+       DATE_FORMAT(AB.to_date  , '%d %b %Y') `To`,
+       utl_calcVacDays(AB.id) `Days`
+  FROM absences AB,
+       absence_types ABT
+ WHERE AB.`type` = ABT.id
+   AND AB.`type` NOT IN (10, 11) -- planned and compensation
+   AND AB.employee_id IN (SELECT id
+                            FROM employees
+                           WHERE person_id IN (SELECT id
+                                                 FROM persons
+                                                WHERE CONCAT(first_name, ' ', IFNULL(middle_name, ''), ' ', last_name) LIKE '%'))
+   AND AB.from_date >= '2012-01-01'
+ ORDER BY AB.from_date;
