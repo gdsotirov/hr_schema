e30eb3e0a4ecdf16af60c7a9e2419b46cae1f813
Georgi D. Sotirov <gdsotirov@gmail.com>
2013-06-01 21:19:49 +0300

    Fix report (join with table persons)

--- db/reports/birthdays_today.sql
+++ db/reports/birthdays_today.sql
@@ -1,6 +1,8 @@
-SELECT CONCAT(EMP.last_name, ', ', EMP.first_name) full_name,
-       EMP.birth_date,
-       utl_getDateDiffStr(EMP.birth_date, CURDATE()) age
-  FROM employees EMP
- WHERE birth_date IS NOT NULL
-   AND CURDATE() = STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-', MONTH(EMP.birth_date), '-', DAY(EMP.birth_date)), '%Y-%m-%d');
+SELECT CONCAT(PER.last_name, ', ', PER.first_name) full_name,
+       PER.birth_date,
+       utl_getDateDiffStr(PER.birth_date, CURDATE()) age
+  FROM employees EMP,
+       persons   PER
+ WHERE PER.id = EMP.person_id
+   AND PER.birth_date IS NOT NULL
+   AND CURDATE() = STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-', MONTH(PER.birth_date), '-', DAY(PER.birth_date)), '%Y-%m-%d');
