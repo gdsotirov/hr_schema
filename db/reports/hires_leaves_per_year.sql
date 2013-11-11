153d8e38250d75ca787dfa5347207949da1d6f40
Georgi D. Sotirov <gdsotirov@gmail.com>
2013-11-11 14:46:08 +0200

    Rewrite the report with UNION

--- db/reports/hires_leaves_per_year.sql
+++ db/reports/hires_leaves_per_year.sql
@@ -1,12 +1,12 @@
-SELECT H.yr `Year`, hires `Hires`, IFNULL(lefts, 0) `Leaves`, hires - IFNULL(lefts, 0) balance
-  FROM (SELECT YEAR(hire_date) yr, count(*) hires
+SELECT yr `Year`, SUM(hires) `Hires`, SUM(`leaves`) `Leaves`, SUM(hires) - SUM(`leaves`) Balance
+  FROM (SELECT YEAR(hire_date) yr, count(*) hires, 0        `leaves`
           FROM employees
-         GROUP BY YEAR(hire_date)
-       ) H
-       LEFT OUTER JOIN
-       (SELECT YEAR(leave_date) yr, count(*) lefts
+         GROUP BY yr
+        UNION
+        SELECT YEAR(leave_date) yr, 0       hires, count(*) `leaves`
           FROM employees
          WHERE leave_date IS NOT NULL
-         GROUP BY 1
-       ) L ON H.yr = L.yr
- ORDER BY H.yr;
+         GROUP BY yr
+       ) HL
+ GROUP BY yr
+ ORDER BY yr DESC;
