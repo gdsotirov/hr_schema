e053361dc11c412e4f1de2b5af3c91fd287be13c
Georgi D. Sotirov <gdsotirov@gmail.com>
2013-12-24 15:56:44 +0200

    Use SUM instead of COUNT

--- db/reports/hires_leaves_per_year.sql
+++ db/reports/hires_leaves_per_year.sql
@@ -1,12 +1,16 @@
-SELECT yr `Year`, SUM(hires) `Hires`, SUM(`leaves`) `Leaves`, SUM(hires) - SUM(`leaves`) Balance
-  FROM (SELECT YEAR(hire_date) yr, count(*) hires, 0        `leaves`
+SELECT HL.yr                    `Year`,
+       SUM(IFNULL(HL.hires, 0)) `Hires`,
+       SUM(IFNULL(HL.lefts, 0)) `Leaves`,
+       SUM(IFNULL(HL.hires, 0)) -
+       SUM(IFNULL(HL.lefts, 0)) `Balance`
+  FROM (SELECT YEAR(hire_date) yr, sum(1) hires, SUM(0) lefts
           FROM employees
-         GROUP BY yr
+         GROUP BY YEAR(hire_date)
         UNION
-        SELECT YEAR(leave_date) yr, 0       hires, count(*) `leaves`
+        SELECT YEAR(leave_date) yr, SUM(0) hires, SUM(1) lefts
           FROM employees
          WHERE leave_date IS NOT NULL
-         GROUP BY yr
+        GROUP BY YEAR(leave_date)
        ) HL
- GROUP BY yr
- ORDER BY yr DESC;
+ GROUP BY HL.yr
+ ORDER BY HL.yr;
