06c046619f74696a8d4f1c0e3016ab1fb299e1f0
Georgi D. Sotirov <gdsotirov@gmail.com>
2014-11-07 09:27:27 +0200

    List persons that are not employees

---
+++ db/reports/persons_not_employees.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(PER.first_name, ' ',
+              CASE
+                WHEN PER.middle_name IS NOT NULL THEN CONCAT(PER.middle_name, ' ')
+                ELSE ''
+              END,
+              PER.last_name) nam
+  FROM persons PER
+       LEFT OUTER JOIN employees EMP on EMP.person_id = PER.id
+ WHERE EMP.id IS NULL;
