416c0ff5eaa788d38ecdaa3fb88b3b0be4501071
Georgi D. Sotirov <gdsotirov@gmail.com>
2015-02-04 10:16:04 +0200

    Calculate from and to dates, so they are in the selected period, select only authorized records

--- db/reports/emp_leaves.sql
+++ db/reports/emp_leaves.sql
@@ -1,4 +1,10 @@
-SELECT CONCAT(PER.first_name, ' ', PER.last_name) nam, A.from_date, utl_calcVacDays(A.id) dur, A.to_date, ABT.title, A.`status`
+SELECT CONCAT(PER.first_name, ' ', PER.last_name) nam,
+       ABT.title,
+       GREATEST(A.from_date, STR_TO_DATE('2015-02-01 00:00:00', '%Y-%m-%d %H:%i:%s')) from_date,
+       LEAST(A.to_date     , STR_TO_DATE('2015-02-28 23:59:59', '%Y-%m-%d %H:%i:%s')) to_date,
+       DATEDIFF(   LEAST(A.to_date  , STR_TO_DATE('2015-02-28 00:00:00', '%Y-%m-%d %H:%i:%s')),
+                GREATEST(A.from_date, STR_TO_DATE('2015-02-01 00:00:00', '%Y-%m-%d %H:%i:%s'))) + 1 days,
+       utl_calcVacDays(A.from_date, A.to_date) total_days
   FROM absences      A,
        absence_types ABT,
        employees     EMP,
@@ -7,16 +13,13 @@ SELECT CONCAT(PER.first_name, ' ', PER.last_name) nam, A.from_date, utl_calcVacD
    AND EMP.department_id = 1
    AND A.`type` NOT IN (10, 11) /* exclude compensations and planned paid leaves */
    /*AND CONCAT(PER.first_name, ' ', PER.last_name) LIKE '%%'*/
-   AND (   (    A.from_date >= STR_TO_DATE('2014-05-01 00:00:00', '%Y-%m-%d %H:%i:%s')
-            AND A.from_date <= STR_TO_DATE('2014-05-31 23:59:59', '%Y-%m-%d %H:%i:%s')
-           )
-        OR (    A.to_date   >= STR_TO_DATE('2014-05-01 00:00:00', '%Y-%m-%d %H:%i:%s')
-            AND A.to_date   <= STR_TO_DATE('2014-05-31 23:59:59', '%Y-%m-%d %H:%i:%s')
-           )
-        OR (    A.from_date <  STR_TO_DATE('2014-05-01 00:00:00', '%Y-%m-%d %H:%i:%s')
-            AND A.to_date   >  STR_TO_DATE('2014-05-31 23:59:59', '%Y-%m-%d %H:%i:%s')
+   AND (   A.from_date BETWEEN STR_TO_DATE('2015-02-01 00:00:00', '%Y-%m-%d %H:%i:%s') AND STR_TO_DATE('2015-02-28 23:59:59', '%Y-%m-%d %H:%i:%s')
+        OR A.to_date   BETWEEN STR_TO_DATE('2015-02-01 00:00:00', '%Y-%m-%d %H:%i:%s') AND STR_TO_DATE('2015-02-28 23:59:59', '%Y-%m-%d %H:%i:%s')
+        OR (    A.from_date <= STR_TO_DATE('2015-02-01 00:00:00', '%Y-%m-%d %H:%i:%s') AND STR_TO_DATE('2015-02-28 23:59:59', '%Y-%m-%d %H:%i:%s')
+            AND A.to_date   >= STR_TO_DATE('2015-02-01 00:00:00', '%Y-%m-%d %H:%i:%s') AND STR_TO_DATE('2015-02-28 23:59:59', '%Y-%m-%d %H:%i:%s')
            )
        )
    AND ABT.id = A.`type`
    AND PER.id = EMP.person_id
- ORDER BY nam, A.from_date;
+   AND A.`status` = 'Authorized'
+ ORDER BY nam, from_date;
