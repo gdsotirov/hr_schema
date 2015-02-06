94dbc95e51661964d9bae2b693891ef13a2d6814
Georgi D. Sotirov <gdsotirov@gmail.com>
2015-02-06 19:07:57 +0200

    Add report with the number of days per absence type of an employee per year

---
+++ db/reports/emp_abs_type_yr_totals.sql
@@ -0,0 +1,21 @@
+SELECT emp_getFullName(EMP.id) `Name`,
+       YEAR(ABS.from_date) `Year`,
+       SUM(IF(ABS.`type` = 1            , utl_calcVacDaysId(ABS.id), 0)) `Paid`,
+       SUM(IF(ABS.`type` = 13           , utl_calcVacDaysId(ABS.id), 0)) `KP`,
+       SUM(IF(ABS.`type` = 4            , utl_calcVacDaysId(ABS.id), 0)) `Others (paid)`,
+       SUM(IF(ABS.`type` IN (2, 3)      , utl_calcVacDaysId(ABS.id), 0)) `Unpaid`,
+       SUM(IF(ABS.`type` = 7            , utl_calcVacDaysId(ABS.id), 0)) `Sick`,
+       SUM(IF(ABS.`type` = 8            , utl_calcVacDaysId(ABS.id), 0)) `Maternity`,
+       SUM(IF(ABS.`type` = 9            , utl_calcVacDaysId(ABS.id), 0)) `Paternity`,
+       SUM(IF(ABS.`type` = 5            , utl_calcVacDaysId(ABS.id), 0)) `Trip`,
+       SUM(IF(ABS.`type` = 6            , utl_calcVacDaysId(ABS.id), 0)) `FromHome`,
+       SUM(IF(ABS.`type` = 12           , utl_calcVacDaysId(ABS.id), 0)) `Compensation`,
+       SUM(IF(ABS.`type` NOT IN (10, 11), utl_calcVacDaysId(ABS.id), 0)) `Total`
+  FROM employees     EMP,
+       absences      ABS,
+       absence_types ABT
+ WHERE ABS.employee_id = EMP.id
+   AND ABS.status = 'Authorized'
+   AND ABT.id = ABS.`type`
+   AND EMP.id = 9
+ GROUP BY `Year`;
