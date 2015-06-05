e1f6b5b87488699b94292055ab34bc2fc79b1e10
Georgi D. Sotirov <gdsotirov@gmail.com>
2015-06-05 10:56:31 +0300

    Calculate annual taxable salary amount instead of monthly as taxes are applied on annual base

--- db/routines/calcNetSalaryTN.sql
+++ db/routines/calcNetSalaryTN.sql
@@ -4,59 +4,63 @@ CREATE FUNCTION calcNetSalaryTN(dGrossSalary DECIMAL) RETURNS DECIMAL(10,2)
 BEGIN
   /* See http://taxsummaries.pwc.com/uk/taxsummaries/wwts.nsf/ID/Tunisia-Individual-Income-determination */
   /* See http://taxsummaries.pwc.com/uk/taxsummaries/wwts.nsf/ID/Tunisia-Individual-Taxes-on-personal-income */
-  DECLARE dSocSec     DECIMAL(10,2);
-  DECLARE dProfExp    DECIMAL(10,2);
-  DECLARE dTaxSal     DECIMAL(10,2);
-  DECLARE dNetSalary  DECIMAL(10,2);
-  DECLARE dIncTaxBase DECIMAL(10,2);
-  DECLARE dIncTaxAmt  DECIMAL(10,2) DEFAULT 0;
+  DECLARE dAnnGrossSal  DECIMAL(10,2);   
+  DECLARE dAnnSocSec    DECIMAL(10,2);
+  DECLARE dMthSocSec    DECIMAL(10,2);
+  DECLARE dAnnProfExp   DECIMAL(10,2);
+  DECLARE dAnnTaxSal    DECIMAL(10,2);
+  DECLARE dAnnTaxAmt    DECIMAL(10,2) DEFAULT 0;
+  DECLARE dMthTaxAmt    DECIMAL(10,2);
+  DECLARE dMthNetSalary DECIMAL(10,2);
 
-  /* Calcualte social contributions */
-  SET dSocSec  = ROUND(dGrossSalary * 9.18 / 100, 2);
-  /* Calcaulte professional expenses */
-  SET dProfExp = ROUND((dGrossSalary - dSocSec) * 10 / 100, 2);
-  /* Calcualte taxable amount by deducting social contributions and professional expenses */
-  SET dTaxSal  = ROUND(dGrossSalary - dSocSec - dProfExp, 2);
-
-  SET dIncTaxBase = dTaxSal;
+  /* Calcualte annual gross salary */
+  SET dAnnGrossSal = dGrossSalary * 12;
+  /* Calcualte annual and monthly social contributions */
+  SET dAnnSocSec = ROUND(dAnnGrossSal * 9.18 / 100, 2);
+  SET dMthSocSec = ROUND(dAnnSocSec / 12, 2);
+  /* Calcaulte annual professional expenses */
+  SET dAnnProfExp = ROUND((dAnnGrossSal - dAnnSocSec) * 10 / 100, 2);
+  /* Calcualte taxable salary amount by deducting annul social contributions and annual professional expenses */
+  SET dAnnTaxSal = ROUND(dAnnGrossSal - dAnnSocSec - dAnnProfExp - 150 - 90 - 75, 2);
 
   /* Apply progressive personal income scale */
-  IF dIncTaxBase <= 1500 THEN
-    SET dIncTaxAmt = 0;
+  IF dAnnTaxSal <= 1500 THEN
+    SET dAnnTaxAmt = 0;
+    SET dMthTaxAmt = 0;
   ELSE
-    IF dIncTaxBase <= 5000 THEN
-      SET dIncTaxAmt = dIncTaxAmt + ROUND((dIncTaxBase - 1500) * 15 / 100, 2);
+    IF dAnnTaxSal <= 5000 THEN
+      SET dAnnTaxAmt = dAnnTaxAmt + ROUND((dAnnTaxSal - 1500) * 15 / 100, 2);
     ELSE
-      SET dIncTaxAmt = dIncTaxAmt + ROUND((5000 - 1500) * 15 / 100, 2);
-      SET dIncTaxBase = dIncTaxBase - 5000;
+      SET dAnnTaxAmt = dAnnTaxAmt + ROUND((5000 - 1500) * 15 / 100, 2);
 
-      IF dIncTaxBase <= 10000 THEN
-        SET dIncTaxAmt = dIncTaxAmt + ROUND((dIncTaxBase - 5000) * 20 / 100, 2);
-    ELSE
-        SET dIncTaxAmt = dIncTaxAmt + ROUND((10000 - 5000) * 20 / 100, 2);
-        SET dIncTaxBase = dIncTaxBase - 10000;
+      IF dAnnTaxSal <= 10000 THEN
+        SET dAnnTaxAmt = dAnnTaxAmt + ROUND((dAnnTaxSal - 5000) * 20 / 100, 2);
+      ELSE
+        SET dAnnTaxAmt = dAnnTaxAmt + ROUND((10000 - 5000) * 20 / 100, 2);
 
-        IF dIncTaxBase <= 20000 THEN
-          SET dIncTaxAmt = dIncTaxAmt + ROUND((dIncTaxBase - 10000) * 25 / 100, 2);
+        IF dAnnTaxSal <= 20000 THEN
+          SET dAnnTaxAmt = dAnnTaxAmt + ROUND((dAnnTaxSal - 10000) * 25 / 100, 2);
         ELSE
-          SET dIncTaxAmt = dIncTaxAmt + ROUND((20000 - 10000) * 25 / 100, 2);
-          SET dIncTaxBase = dIncTaxBase - 20000;
+          SET dAnnTaxAmt = dAnnTaxAmt + ROUND((20000 - 10000) * 25 / 100, 2);
 
-          IF dIncTaxBase <= 50000 THEN
-            SET dIncTaxAmt = dIncTaxAmt + ROUND((dIncTaxBase - 10000) * 30 / 100, 2);
+          IF dAnnTaxSal <= 50000 THEN
+            SET dAnnTaxAmt = dAnnTaxAmt + ROUND((dAnnTaxSal - 20000) * 30 / 100, 2);
           ELSE
-            SET dIncTaxAmt = dIncTaxAmt + ROUND((50000 - 20000) * 30 / 100, 2);
-            SET dIncTaxAmt = dIncTaxAmt + ROUND(dIncTaxBase - 50000 * 35 / 100, 2);
+            SET dAnnTaxAmt = dAnnTaxAmt + ROUND((50000 - 20000) * 30 / 100, 2);
+            SET dAnnTaxAmt = dAnnTaxAmt + ROUND(dAnnTaxSal - 50000 * 35 / 100, 2);
           END IF;
         END IF;
       END IF;
     END IF;
+
+	/* Calcualte monthly income tax */
+    SET dMthTaxAmt = ROUND(dAnnTaxAmt / 12, 2);
   END IF;
 
   /* Calcualte Net Salary */
-  SET dNetSalary = dTaxSal - dIncTaxAmt;
+  SET dMthNetSalary = dGrossSalary - dMthSocSec - dMthTaxAmt;
 
-  RETURN dNetSalary;
+  RETURN dMthNetSalary;
 END //
 
 DELIMITER ;
