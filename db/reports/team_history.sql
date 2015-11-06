1cfd5d53d5b09a99075a12f5417257ce164e40c7
Georgi D. Sotirov <gdsotirov@gmail.com>
2015-11-06 13:58:25 +0200

    Use functions getTeamJoinDate and getTeamExitDate to list when an employee joined or exited the department, select also previous members of the department

--- db/reports/team_history.sql
+++ db/reports/team_history.sql
@@ -1,11 +1,27 @@
-SELECT EMP.id, emp_getFullName(EMP.id) full_name,
-       EMP.hire_date, EMP.leave_date,
-       CASE
-         WHEN EMP.leave_date IS NOT NULL THEN utl_getDateDiffStr(EMP.hire_date, EMP.leave_date)
-         ELSE 'n/a'
-       END service
+SELECT EMP.id, PER.first_name, PER.last_name,
+       PER.birth_date,
+       EMP.hire_date,
+       getTeamJoinDate(EMP.id, DEP.id)  joined_on,
+       getTeamExitDate(EMP.id, DEP.id)  exited_on,
+       utl_getDateDiffStr(getTeamJoinDate(EMP.id, DEP.id),
+                          IFNULL(getTeamExitDate(EMP.id, DEP.id), NOW()))
+                                        in_the_team,
+       EMP.leave_date,
+       emp_getServiceLength(EMP.id)     total_service
   FROM employees   EMP,
-       departments DEP
- WHERE DEP.id = EMP.department_id
-   AND DEP.`name` = 'Accounting'
- ORDER BY EMP.hire_date DESC;
+       departments DEP,
+       persons     PER
+ WHERE (   DEP.id = EMP.department_id /* currently in the department */
+           /* or have been member of the department */
+        OR DEP.id IN (SELECT department
+                        FROM team_change
+                       WHERE employee_id = EMP.id
+                     )
+       )
+   AND PER.id = EMP.person_id
+   AND DEP.`name` LIKE '%Accounting%'
+   /*AND EMP.leave_date IS NULL
+   AND getTeamExitDate(EMP.id, DEP.id) IS NULL*/
+   /*AND EMP.leave_date IS [NOT] NULL*/
+   /*AND getTeamExitDate(EMP.id, DEP.id) IS NULL*/
+ ORDER BY joined_on DESC;
