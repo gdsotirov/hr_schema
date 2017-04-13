e8a8078cefe7051e41b2d0c30df2793a9c32e42f
Georgi D. Sotirov <gdsotirov@gmail.com>
2017-04-13 12:37:18 +0300

    Select only approved bonuses

--- db/reports/list_tot_bonus.sql
+++ db/reports/list_tot_bonus.sql
@@ -1,8 +1,10 @@
-SELECT EMP.id, CONCAT(PER.first_name, ' ', PER.last_name) nam, SUM(amount) total
+SELECT EMP.id, CONCAT(PER.first_name, ' ', PER.last_name) nam, SUM(amount) total, COUNT(*) cnt
   FROM bonus_distribution BD,
        employees          EMP,
        persons            PER
  WHERE BD.employee_id = EMP.id
    AND PER.id = EMP.person_id
+   AND BD.approved_on IS NOT NULL
+   /*AND BD.approved_on < '2017-03-29'*/
  GROUP BY EMP.id
- ORDER BY total desc;
+ ORDER BY total DESC;
