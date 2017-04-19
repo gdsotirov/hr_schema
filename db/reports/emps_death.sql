d0d5412342c6c5915d827a8c6a0aa634a3680da7
Georgi D. Sotirov <gdsotirov@gmail.com>
2017-04-19 15:28:26 +0300

    List employees who died on the job

---
+++ db/reports/emps_death.sql
@@ -0,0 +1,15 @@
+SELECT CONCAT(PER.first_name, ' ', PER.last_name) nam,
+       PER.birth_date, EMP.hire_date,
+       (SELECT MIN(hire_date)
+          FROM employees
+         WHERE person_id = PER.id) first_hired,
+       PER.death_date,
+       TIMESTAMPDIFF(Year, PER.birth_date, PER.death_date) age,
+       utl_getDateDiffStr(PER.birth_date, PER.death_date) age_full,
+       utl_getDateDiffStr(EMP.hire_date, EMP.leave_date) intership,
+       NULL total_intership
+  FROM persons   PER,
+       employees EMP
+ WHERE EMP.person_id = PER.id
+   AND EMP.leave_date = PER.death_date
+ ORDER BY PER.death_date DESC;
