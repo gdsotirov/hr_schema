1a1d554676e96bc21c9487daebb778f7728589b2
Georgi D. Sotirov <gdsotirov@gmail.com>
2017-10-23 14:35:00 +0300

    Add linkedin_profile into the report

--- db/reports/persons_not_employees.sql
+++ db/reports/persons_not_employees.sql
@@ -3,7 +3,8 @@ SELECT CONCAT(PER.first_name, ' ',
                 WHEN PER.middle_name IS NOT NULL THEN CONCAT(PER.middle_name, ' ')
                 ELSE ''
               END,
-              PER.last_name) nam
+              PER.last_name) nam,
+       PER.linkedin_profile
   FROM persons PER
        LEFT OUTER JOIN employees EMP on EMP.person_id = PER.id
  WHERE EMP.id IS NULL;
