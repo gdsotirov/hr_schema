7116f8edf903586e94b623a5f63275df8a801ee3
Georgi D. Sotirov <gdsotirov@gmail.com>
2017-11-27 11:15:48 +0200

    Compare month and day, calculate only again in years

--- db/reports/birthdays_today.sql
+++ db/reports/birthdays_today.sql
@@ -1,8 +1,9 @@
-SELECT CONCAT(PER.last_name, ', ', PER.first_name) full_name,
+SELECT CONCAT(PER.first_name, ' ', PER.last_name) nam,
        PER.birth_date,
-       utl_getDateDiffStr(PER.birth_date, CURDATE()) age
+       YEAR(CURDATE()) - YEAR(PER.birth_date) age
   FROM employees EMP,
        persons   PER
  WHERE PER.id = EMP.person_id
    AND PER.birth_date IS NOT NULL
-   AND CURDATE() = STR_TO_DATE(CONCAT(YEAR(CURDATE()), '-', MONTH(PER.birth_date), '-', DAY(PER.birth_date)), '%Y-%m-%d');
+   AND EXTRACT(DAY   FROM PER.birth_date) = EXTRACT(DAY   FROM CURDATE())
+   AND EXTRACT(MONTH FROM PER.birth_date) = EXTRACT(MONTH FROM CURDATE());
