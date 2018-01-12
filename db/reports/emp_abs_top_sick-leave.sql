374f0a0c15816773aa4e7f2e5b7fe3a165042dc4
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-01-12 10:13:55 +0200

    Add internship, average days and average days per internship in the report, exclude canceled absences

--- db/reports/emp_abs_top_sick-leave.sql
+++ db/reports/emp_abs_top_sick-leave.sql
@@ -1,5 +1,9 @@
 SELECT CONCAT(PER.first_name, ' ', PER.last_name) emp_name,
-       SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) tot_dur
+       utl_getDateDiffStr(EMP.hire_date, IFNULL(EMP.leave_date, CURDATE())) intership,
+       SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) tot_dur,
+       ROUND(AVG(utl_calcVacDays(SLV.from_date, SLV.to_date)), 2) avg_days,
+       ROUND(SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) /
+             (TIMESTAMPDIFF(YEAR, EMP.hire_date, IFNULL(EMP.leave_date, CURDATE())) + 1), 2) avg_days_per_year
   FROM employees EMP,
        persons   PER,
        absences  SLV /* Sick Leave */
@@ -7,5 +11,6 @@ SELECT CONCAT(PER.first_name, ' ', PER.last_name) emp_name,
    AND EMP.id = SLV.employee_id
    AND SLV.`type` = 7 /* sick leave */
    AND EMP.department_id = 1 /* Accounting */
+   AND SLV.`status` != 'Cancelled'
  GROUP BY emp_name
  ORDER BY tot_dur DESC;
