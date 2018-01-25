26fc149118dfe6c07ef68b21b2e01068642bf98f
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-01-25 16:01:24 +0200

    Add internship to leaving employees' report

--- db/reports/emp_leaving.sql
+++ db/reports/emp_leaving.sql
@@ -1,16 +1,19 @@
-SELECT CONCAT(PER.first_name, ' ',
+SELECT EMP.id,
+       CONCAT(PER.first_name, ' ',
               CASE
                 WHEN PER.middle_name IS NOT NULL THEN CONCAT(PER.middle_name, ' ')
                 ELSE ''
               END,
               PER.last_name) emp_name,
        DEP.`name` dep_name,
+       EMP.hire_date,
        EMP.leave_date,
+       utl_getDateDiffStr(EMP.hire_date, NOW()) internship,
        utl_getDateDiffStr(NOW(), EMP.leave_date) leaving_in
   FROM persons     PER,
        employees   EMP,
        departments DEP
  WHERE EMP.person_id = PER.id
    AND EMP.department_id = DEP.id
-   AND EMP.leave_date >= DATE(NOW()) /*STR_TO_DATE('2016-12-01', '%Y-%m-%d')*/
+   AND EMP.leave_date >= CURDATE()/*STR_TO_DATE('2017-11-01', '%Y-%m-%d')*/
  ORDER BY EMP.leave_date;
