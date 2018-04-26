676d566a9d8db5fc1bdbff5117fdd44a5d2d8f0b
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-04-26 13:35:03 +0300

    Rewrite the report with single query using GROUPING function

--- db/reports/divisions_size.sql
+++ db/reports/divisions_size.sql
@@ -1,13 +1,12 @@
-SELECT DV.id, DV.`name`, COUNT(*) emp_cnt
-  FROM divisions DV,
-       employees EMP
- WHERE DV.id = EMP.division_id
-   AND (EMP.leave_date IS NULL OR EMP.leave_date > DATE(NOW()))
-   AND EMP.hire_date <= DATE(NOW())
- GROUP BY `name`
-UNION ALL
-SELECT 9 id, 'Total' `name`, COUNT(*) emp_cnt
-  FROM employees EMP
- WHERE EMP.leave_date IS NULL OR EMP.leave_date > DATE(NOW())
-   AND EMP.hire_date <= DATE(NOW())
- ORDER BY id;
+SELECT CASE GROUPING(DV.id)     WHEN 1 THEN 99      ELSE DV.id     END id,
+       CASE GROUPING(DV.`name`) WHEN 1 THEN 'Total' ELSE DV.`name` END dept,
+       COUNT(EMP.id) emp_cnt
+  FROM divisions DV
+       LEFT OUTER JOIN employees EMP
+         ON DV.id = EMP.division_id
+        AND (EMP.leave_date IS NULL OR EMP.leave_date > DATE(NOW()))
+        AND EMP.hire_date <= DATE(NOW())
+ WHERE 1=1
+ GROUP BY DV.id, DV.`name` WITH ROLLUP
+HAVING    (GROUPING(DV.id) = 0 AND GROUPING(DV.`name`) = 0)
+       OR (GROUPING(DV.id) = 1 AND GROUPING(DV.`name`) = 1);
