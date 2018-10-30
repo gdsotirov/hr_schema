08c61f08bb8f4c787f1babae32f1e01ac959e85b
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-10-30 14:37:13 +0200

    Rank employees by total duration of sick leaves

--- db/reports/emp_abs_top_sick-leave.sql
+++ db/reports/emp_abs_top_sick-leave.sql
@@ -1,4 +1,5 @@
-SELECT CONCAT(PER.first_name, ' ', PER.last_name) emp_name,
+SELECT DENSE_RANK() OVER (ORDER BY SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) DESC) AS rnk,
+       CONCAT(PER.first_name, ' ', PER.last_name) emp_name,
        utl_getDateDiffStr(EMP.hire_date, IFNULL(EMP.leave_date, CURDATE())) intership,
        SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) tot_dur,
        ROUND(AVG(utl_calcVacDays(SLV.from_date, SLV.to_date)), 2) avg_days,
@@ -12,5 +13,5 @@ SELECT CONCAT(PER.first_name, ' ', PER.last_name) emp_name,
    AND SLV.`type` = 7 /* sick leave */
    AND EMP.department_id = 1 /* Accounting */
    AND SLV.`status` != 'Cancelled'
- GROUP BY emp_name
+ GROUP BY EMP.id
  ORDER BY tot_dur DESC;
