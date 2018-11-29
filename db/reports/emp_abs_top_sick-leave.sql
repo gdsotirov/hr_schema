35474dbb43365a8c1df01f501e292132e11736a5
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-11-29 09:37:43 +0200

    Rename column avg_days to avg_days_per_leave

--- db/reports/emp_abs_top_sick-leave.sql
+++ db/reports/emp_abs_top_sick-leave.sql
@@ -2,7 +2,7 @@ SELECT DENSE_RANK() OVER (ORDER BY SUM(utl_calcVacDays(SLV.from_date, SLV.to_dat
        CONCAT(PER.first_name, ' ', PER.last_name) emp_name,
        utl_getDateDiffStr(EMP.hire_date, IFNULL(EMP.leave_date, CURDATE())) intership,
        SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) tot_dur,
-       ROUND(AVG(utl_calcVacDays(SLV.from_date, SLV.to_date)), 2) avg_days,
+       ROUND(AVG(utl_calcVacDays(SLV.from_date, SLV.to_date)), 2) avg_days_per_leave,
        ROUND(SUM(utl_calcVacDays(SLV.from_date, SLV.to_date)) /
              (TIMESTAMPDIFF(YEAR, EMP.hire_date, IFNULL(EMP.leave_date, CURDATE())) + 1), 2) avg_days_per_year
   FROM employees EMP,
