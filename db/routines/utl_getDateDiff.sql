75ac9b75da0efe4e272b6331c3dbdaf20df695ad
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-02-08 12:09:19 +0200

    Procedure for calculation of difference between two dates in years, months and days

---
+++ db/routines/utl_getDateDiff.sql
@@ -0,0 +1,19 @@
+﻿DELIMITER //
+
+CREATE PROCEDURE utl_getDateDiff(IN date1 DATE, IN date2 DATE, OUT yrs INT, OUT mnths INT, OUT dys INT)
+  NO SQL
+  DETERMINISTIC
+BEGIN
+  DECLARE years INTEGER;
+  DECLARE months INTEGER;
+  DECLARE days INTEGER;
+
+  /* Years between */
+  SET yrs   := TIMESTAMPDIFF(YEAR, date1, date2);
+  /* Months between */
+  SET mnths := TIMESTAMPDIFF(MONTH, DATE_ADD(date1, INTERVAL years YEAR), date2);
+  /* Days between */
+  SET dys   := TIMESTAMPDIFF(DAY, DATE_ADD(date1, INTERVAL years * 12 + months MONTH), date2);
+END //
+
+DELIMITER ;
