f5f7fc5dc4e273f34d2ddf1b271ba40930bb4cc8
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-02-08 16:00:48 +0200

    Modify report to display also seniority years, gross salary, and calculate net salary considering seniority amount

--- db/reports/team_salaries.sql
+++ db/reports/team_salaries.sql
@@ -2,9 +2,15 @@ SELECT EMP.id, concat(PER.first_name, ' ', PER.last_name) emp_name,
        utl_getDateDiffStr(EMP.hire_date, NOW()) inership,
        (SELECT MAX(from_date) from sal_history WHERE employee_id = EMP.id) last_adjustment,
        EMP.salary base_sal, EMP.currency,
+       emp_calcSnrtyYrs(EMP.id) snrty_yrs,
        CASE
          WHEN EMP.division_id = 1 /* BG */ THEN
-           calcNetSalaryBG(EMP.salary)
+            EMP.salary + caclSnrtyAmt(EMP.salary, emp_calcSnrtyYrs(EMP.id))
+         ELSE EMP.salary
+       END gross_salary, EMP.currency,
+       CASE
+         WHEN EMP.division_id = 1 /* BG */ THEN
+           calcNetSalaryBG(EMP.salary + caclSnrtyAmt(EMP.salary, emp_calcSnrtyYrs(EMP.id)))
          WHEN EMP.division_id = 4 /* VN */ THEN
            calcNetSalaryVN(EMP.salary)
          WHEN EMP.division_id = 5 /* TN */ THEN
@@ -14,7 +20,7 @@ SELECT EMP.id, concat(PER.first_name, ' ', PER.last_name) emp_name,
        EMP.currency,
        ROUND(curr_conversion(CASE
                                WHEN EMP.division_id = 1 /* BG */ THEN
-                                 calcNetSalaryBG(EMP.salary)
+                                 calcNetSalaryBG(EMP.salary + caclSnrtyAmt(EMP.salary, emp_calcSnrtyYrs(EMP.id)))
                                WHEN EMP.division_id = 4 /* VN */ THEN
                                  calcNetSalaryVN(EMP.salary)
                                WHEN EMP.division_id = 5 /* TN */ THEN
